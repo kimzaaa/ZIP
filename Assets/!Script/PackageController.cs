9f97705075bba4c8ad37887a2abe6a048f266926
PEPSEALSEA <iampep2009@gmail.com>
2025-04-23 17:30:19 +0700

    Update PackageController.cs

--- Assets/!Script/PackageController.cs
+++ Assets/!Script/PackageController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 [RequireComponent(typeof(PlayerController2))]
 public class PackageController : MonoBehaviour
@@ -9,13 +11,17 @@ public class PackageController : MonoBehaviour
     [SerializeField] private bool isDamaged = false;
     [SerializeField] private GameObject packageVisual;
 
+    [Header("UI Elements")]
+    [SerializeField] private Slider healthSlider; // Reference to the UI Slider
+    [SerializeField] private float healthAnimationDuration = 0.5f; // Duration for DOTween animation
+
     [Header("Effects")]
     [SerializeField] private GameObject damageEffect;
     [SerializeField] private GameObject healEffect;
     [SerializeField] private GameObject deliveryEffect;
 
     private PlayerController2 playerController;
-    private bool hasPackage = true;
+    public bool hasPackage = true;
 
     private void Awake()
     {
@@ -26,9 +32,17 @@ public class PackageController : MonoBehaviour
 
     private void Start()
     {
-        //Start with no package
+        // Start with no package
         if (packageVisual.activeSelf) packageVisual.SetActive(false);
-        
+
+        // Initialize the slider
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = currentHealth;
+            healthSlider.gameObject.SetActive(false); // Initially disable slider
+        }
+
         if (ScoreManager.Instance != null)
         {
             ScoreManager.Instance.packageHP = currentHealth;
@@ -54,12 +68,25 @@ public class PackageController : MonoBehaviour
 
         currentHealth -= damage;
 
+        // Animate the slider value
+        if (healthSlider != null && healthSlider.gameObject.activeSelf)
+        {
+            healthSlider.DOValue(currentHealth, healthAnimationDuration).SetEase(Ease.OutQuad);
+        }
+
         if (currentHealth <= 0)
         {
             currentHealth = 0;
             isDamaged = true;
+            hasPackage = false; // Set hasPackage to false
 
-            //Package visual on the back
+            // Disable the slider
+            if (healthSlider != null)
+            {
+                healthSlider.gameObject.SetActive(false);
+            }
+
+            // Package visual on the back
             if (packageVisual.activeSelf) packageVisual.SetActive(false);
             if (damageEffect != null)
             {
@@ -78,11 +105,17 @@ public class PackageController : MonoBehaviour
         currentHealth = maxHealth;
         isDamaged = false;
         hasPackage = true;
-        
+
         if (packageVisual != null)
         {
             packageVisual.SetActive(true);
-            
+        }
+
+        // Enable and animate the slider value
+        if (healthSlider != null)
+        {
+            healthSlider.gameObject.SetActive(true);
+            healthSlider.DOValue(currentHealth, healthAnimationDuration).SetEase(Ease.OutQuad);
         }
 
         if (healEffect != null)
@@ -137,6 +170,12 @@ public class PackageController : MonoBehaviour
                 if (packageVisual != null)
                 {
                     packageVisual.SetActive(false);
+                }
+
+                // Disable the slider
+                if (healthSlider != null)
+                {
+                    healthSlider.gameObject.SetActive(false);
                 }
 
                 if (ScoreManager.Instance != null)
